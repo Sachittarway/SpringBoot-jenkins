4f1e2f75a3233e74a2436969afd32285648feb81
Sachit Tarway <86160848+Sachittarway@users.noreply.github.com>
2025-03-05 10:58:06 +0530

    Update Application.java

--- src/main/java/com/lovetolearn/SpringBootApplication/Application.java
+++ src/main/java/com/lovetolearn/SpringBootApplication/Application.java
@@ -1,27 +1,17 @@
 package com.lovetolearn.SpringBootApplication;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
-	public static final Logger logger = LoggerFactory.getLogger(Application.class);
+    public static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		logger.info("its a continous integration job");
-		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		logger.info("its my second logger");
-		
-	}
-
+    public static void main(String[] args) {
+        logger.info("its a continuous integration job");
+        SpringApplication.run(Application.class, args);
+    }
 }

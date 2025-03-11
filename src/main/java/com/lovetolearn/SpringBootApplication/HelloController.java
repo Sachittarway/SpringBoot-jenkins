fd20b0964684076f5f95ed6a1234dedd7acb2aa9
Sachit Tarway <86160848+Sachittarway@users.noreply.github.com>
2025-03-11 13:54:42 +0530

    Update HelloController.java

--- src/main/java/com/lovetolearn/SpringBootApplication/HelloController.java
+++ src/main/java/com/lovetolearn/SpringBootApplication/HelloController.java
@@ -2,6 +2,7 @@ package com.lovetolearn.SpringBootApplication;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
